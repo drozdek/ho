074de091c02724a99ae35cb36043db1fd1bbe53f
rafal <raf.drozdowski@gmail.com>
2021-03-28 16:29:56 +0100

    styles up inputs adds simple form to add data adds package body-parser to parse data entered

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 var app = express();
+
 var port = 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.post('/addevent', function (req, res) {
-    console.log('post  called');
+    console.log('addevent post called');
+    console.log(req.body.id);
 });
 
 
 app.get('/', function (req, res) {
-    console.log('get called index');
+    console.log('index called');
 })
 
 app.listen(port);
