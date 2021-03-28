b16a22390d9a1a8c543a0621818c786bff47e59c
rafal <raf.drozdowski@gmail.com>
2021-03-28 20:35:23 +0100

    creates an exported instance of database inclues db into main file adjusts look of inputs

--- src/db.js
+++ src/db.js
@@ -1,15 +1,29 @@
-var sqlite3 = require('sqlite3');
+var sqlite3 = require('sqlite3').verbose();
 
-const DB = './database/db.db';
+const DB_NAME = './database/db.db';
 
-new sqlite3.Database(DB, sqlite3.OPEN_READWRITE, err => {
-    if (err) {
-        return console.error('DB opened in W+ mode');
-    }
-});
+let
+    db = new sqlite3.Database(DB_NAME, err => {
+        if (err) {
+            return console.error('DBerror' + err.message);
+        }
+        else {
+            console.log('Connected to DB');
+            db.run(`CREATE TABLE events (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    type text,
+                    actorID INTEGER
+                )`,
+                err => {
+                    if (err) {
+                        console.log('table already crated');
+                    }
+                    else {                        
+                        var insert = 'INSERT INTO events (id, type, actorID) VALUES (?,?,?)'
+                        db.run(insert, [12345, "click", 987])
+                    }
+                }
+            )}
+    });
 
-
-new sqlite3.Database(DB, sqlite3.OPEN_READONLY, (err) => {
-    if (err)
-        return console.error('DB opened in R mode')
-});
+module.exports = db
--- src/index.js
+++ src/index.js
@@ -1,22 +1,40 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
+var db = require('./db');
 var app = express();
+
+var sqlite3 = require('sqlite3').verbose();
 
 var port = 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/addevent', function (req, res) {
-    console.log('addevent post called');
-    console.log(req.body.id);
-});
-
-
-app.get('/', function (req, res) {
-    console.log('index called');
+app.get('/', (req, res) => {
+    var sql = "select * from events";
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            console.error(err.message)
+        }
+        else {
+            res.json({
+                "message": "success",
+                "data": rows
+            })
+        }
+    })
+    console.log('response');
 })
+
+app.post('/addevent', (req, res) => {
+    console.log('addevent post called');
+    var response = [id, type, actorID, actorAvatarUrl, repoID, repoName, repoUrl] = [req.body];
+    res.end(JSON.stringify(response));
+
+
+
+});
 
 app.listen(port);
